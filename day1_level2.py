795d711b62e9e816a3df71ed7e99632a203c4c60
UP240713 <up240713@alumnos.upa.edu.mx>
2025-06-05 10:55:23 -0600

    comentario

---
+++ day1_level2.py
@@ -0,0 +1,17 @@
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+print("Nicolás Zavala Reynoso")
+print("Familia Zavala Reynoso")
+print("Mexico")
+print("I am enjoying 30 dayts of python")
+
+print(type(10))
+print(type(9.88))
+print(type("ASDASD"))
+
